1ae5e66fa63552b7d73e9b76fd0f7e3d3fd2bfd6
Tianren Li <tianrenli92@gmail.com>
2020-02-14 19:54:33 -0500

    week 2 react state

--- src/App.js
+++ src/App.js
@@ -2,29 +2,89 @@ import React, {Component} from 'react';
 import './App.css';
 
 class Course {
-    constructor(college, dept, number) {
+    constructor(college, department, number, section, professor) {
         this.college = college;
-        this.dept = dept;
+        this.department = department;
         this.number = number;
-        this.key = `${this.college} ${this.dept} ${this.number}`;
+        this.section = section;
+        this.professor = professor;
+        this.key = `${college} ${department} ${number} ${section}`;
     }
 }
 
 class App extends Component {
-    render() {
-        let courses = [
-            new Course('CAS', 'CS', '111'),
-            new Course('CFA', 'MP', '113'),
-            new Course('GRS', 'CS', '651'),
+    constructor(props) {
+        super(props);
+        const columns = [
+            'College',
+            'Department',
+            'Number',
+            'Section',
+            'Professor',
+        ]
+
+        const courses = [
+            new Course('CAS', 'CS', '112', 'B1', 'Sullivan'),
+            new Course('CAS', 'CS', '332', 'A1', 'Bun'),
+            new Course('CAS', 'CS', '350', 'A1', 'Sarkar'),
         ];
-        console.log(courses);
+
+        this.state = {
+            columns,
+            courses,
+        };
+        this.addNew = this.addNew.bind(this);
+    }
+
+    addNew() {
+        const course = new Course('CAS', 'CS', '111', 'A1', 'Sullivan');
+        const {courses} = this.state;
+        courses.push(course);
+        this.setState({courses});
+    }
+
+    addNew2 = () => {
+        this.addNew();
+    }
+
+    delete(key) {
+        let {courses} = this.state;
+        courses = courses.filter((course) => course.key !== key);
+        this.setState({courses});
+
+    }
+
+    render() {
+        const {columns, courses} = this.state;
         return (
-            <ol>
-                {
-                    courses.map(course => <li key={course.key}>{course.college} {course.dept} {course.number}</li>)
-                }
-            </ol>
-        );
+            <div className="App">
+                <table>
+                    <thead>
+                    <tr>
+                        {columns.map(column =>
+                            <th key={column}>{column}</th>)}
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        courses.map(course =>
+                            <tr key={course.key}>
+                                <td>{course.college}</td>
+                                <td>{course.department}</td>
+                                <td>{course.number}</td>
+                                <td>{course.section}</td>
+                                <td>{course.professor}</td>
+                                <td>
+                                    <button onClick={() => this.delete(course.key)}>Delete</button>
+                                </td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+                </table>
+                <button onClick={this.addNew2}>Add new course</button>
+            </div>
+        ); // <tr>..</tr>, <tr>..</tr>, <tr>..</tr>, ..
     }
 }
 
